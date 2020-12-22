34d4ec08990e28a788ad9def60747c14c94c578e
Michael Puening <mpuening@gmail.com>
2020-12-21 22:41:53 -0500

    Update to Spring Boot 2.4.1

--- learn-webservices-jaxrpc-server/src/test/java/io/github/learnjaxrpc/LearnJaxRpcServerApplicationTests.java
+++ learn-webservices-jaxrpc-server/src/test/java/io/github/learnjaxrpc/LearnJaxRpcServerApplicationTests.java
@@ -1,7 +1,7 @@
 package io.github.learnjaxrpc;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.net.MalformedURLException;
 import java.rmi.RemoteException;
